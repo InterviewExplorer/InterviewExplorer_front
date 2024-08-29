3c6e32532246727677a930bb45669b7300d383e5
sunguh0904 <sunguh0904@gmail.com>
2024-08-29 10:00:01 +0900

    pdf.js axios 통신, 백단 꼬리물기 질문 생성 중

--- src/pages/Pdf.js
+++ src/pages/Pdf.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useState } from 'react';
 
 function Pdf() {
@@ -49,6 +50,23 @@ function Pdf() {
         } catch (error) {
             console.error('에러 발생:', error);
             alert('질문 생성 중 오류가 발생했습니다.');
+        }
+        try {
+            const response = await axios.post('http://localhost:8000/generate_question/', {
+                job: job,
+                years: years,
+            });
+
+            if (response.status !== 201) {
+                throw new Error('통신에러');
+            }
+
+            const data = response.data;
+            setQuestions(data);
+            setCurrentQuestionIndex(0);
+        } catch (error) {
+            console.error('Axios 에러 발생:', error);
+            alert('꼬리물기 질문 생성 중 오류가 발생했습니다.');
         }
     };
 
