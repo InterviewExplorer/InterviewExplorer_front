0e2a8d23c0db0ddcd40b5608e220dd14e146aa4a
LXXDJ <joker96.dj@gmail.com>
2024-09-11 09:05:47 +0900

    #25 기본 css 설치

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,10 +2,24 @@ import { Link } from 'react-router-dom';
 
 function Main() {
     return (
-        <>
-            <h1>메인페이지</h1>
-            <Link to="/getInfo">면접 시작</Link>
-        </>
+        <div className='ly_all el_bg ly_flexC ly_fitemC'>
+            <div className=''>
+                <h2 className=''>언제 어디서나! 간편하게!</h2>
+                <h1 className='el_lv1Head hp_fontGmarket'>AI 가상 면접 서비스 : Interview Explorer</h1>
+                <div className='hp_mt50'>
+                    <div className='el_box el_box__main'>
+                        <div className='el_box__txt'>면접관 ver</div>
+                        <div className='el_box__icon el_icon__interviewer'></div>
+                        <Link to="/getInfo" className='el_btnM el_btn0Back hp_br99 hp_alignC'>시작하기</Link>
+                    </div>
+                    <div className='el_box el_box__main hp_ml30'>
+                        <div className='el_box__txt'>지원자 ver</div>
+                        <div className='el_box__icon el_icon__interviewee'></div>
+                        <Link to="/getInfo" className='el_btnM el_btn0Back hp_br99 hp_alignC'>시작하기</Link>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
