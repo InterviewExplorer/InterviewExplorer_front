6df33bbbaad288e1bbfd3dc871b175e51cbb22b2
LXXDJ <joker96.dj@gmail.com>
2024-09-13 15:33:57 +0900

    #25 차트 수정

--- src/pages/Chart.js
+++ src/pages/Chart.js
@@ -4,7 +4,7 @@ import React from 'react';
 const MyResponsiveRadar = ({ data }) => (
     <ResponsiveRadar
         data={data}
-        keys={['chardonnay', 'carmenere', 'syrah']}
+        keys={['chardonnay']}
         indexBy="taste"
         valueFormat=">-.2f"
         margin={{ top: 70, right: 80, bottom: 40, left: 80 }}
@@ -16,62 +16,21 @@ const MyResponsiveRadar = ({ data }) => (
         colors={{ scheme: 'nivo' }}
         blendMode="multiply"
         motionConfig="wobbly"
-        legends={[
-            {
-                anchor: 'top-left',
-                direction: 'column',
-                translateX: -50,
-                translateY: -40,
-                itemWidth: 80,
-                itemHeight: 20,
-                itemTextColor: '#999',
-                symbolSize: 12,
-                symbolShape: 'circle',
-                effects: [
-                    {
-                        on: 'hover',
-                        style: {
-                            itemTextColor: '#000'
-                        }
-                    }
-                ]
-            }
-        ]}
     />
 );
 
 function Chart() {
     const data = [
-        {
-            "taste": "한글1",
-            "chardonnay": 60,
-            "carmenere": 40,
-            "syrah": 70
-        },
-        {
-            "taste": "bitter",
-            "chardonnay": 90,
-            "carmenere": 60,
-            "syrah": 40
-        },
-        {
-            "taste": "heavy",
-            "chardonnay": 80,
-            "carmenere": 70,
-            "syrah": 50
-        },
-        {
-            "taste": "strong",
-            "chardonnay": 50,
-            "carmenere": 80,
-            "syrah": 30
-        },
-        {
-            "taste": "sunny",
-            "chardonnay": 40,
-            "carmenere": 60,
-            "syrah": 70
-        }
+        {"taste": "한글1",
+        "chardonnay": 60},
+        {"taste": "bitter",
+        "chardonnay": 90},
+        {"taste": "heavy",
+        "chardonnay": 80,},
+        {"taste": "strong",
+        "chardonnay": 50,},
+        {"taste": "sunny",
+        "chardonnay": 40,}
     ];
 
     return (
