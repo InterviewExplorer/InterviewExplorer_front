5d409783fe784a4aff2526a46d6024800e544e68
LXXDJ <joker96.dj@gmail.com>
2024-09-13 12:54:04 +0900

    Interview_technical.js 삭제

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "./pages/Main";
 import GetInfo from "./pages/GetInfo";
-import Interview_technical from "./pages/Interview_technical";
 import Report from "./pages/Report";
 import Interview from "./pages/Interview";
 import Guide from "./pages/Guide";
@@ -14,7 +13,6 @@ function App() {
           <Route path="/" element={<Main />}/>
           <Route path="/getInfo" element={<GetInfo />} />
           <Route path="/interview" element={<Interview />} />
-          {/* <Route path="/interview_technical" element={<Interview_technical />} /> */}
           <Route path="/report" element={<Report />} />
           <Route path="/guide" element={<Guide/>}/>
         </Routes>
