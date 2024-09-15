fe83f9972c3ebd3eb995a0e02360cfa6aaea5388
LXXDJ <joker96.dj@gmail.com>
2024-09-15 21:17:23 +0900

    #25 메인완료

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -3,13 +3,12 @@ import { Link } from 'react-router-dom';
 function Main() {
     return (
         <div className='ly_all el_bg ly_flexC ly_fitemC'>
-            <div className='ly_flexC ly_fitemC'>
-                {/* <h3 className='el_logo fascinate'>in</h3> */}
-                {/* <h3 className='el_logo2 fascinate'>i</h3> */}
+            <div className='ly_flexC ly_fitemC hp_pr50'>
+                <div className='el_img hp_mr20'></div>
                 <div className='hp_ml70'>
                     <h2 className='el_lv2Head hp_fontGmarket hp_mb5'>언제 어디서나! 간편하게!</h2>
                     <h1 className='el_lv1Head hp_fontGmarket'>AI 면접 도우미 : Interview Explorer</h1>
-                    <div className='hp_mt30'>
+                    <div className='hp_mt50'>
                         <div className='el_box el_box__main'>
                             <div className='el_box__txt'>면접관 ver</div>
                             <div className='el_box__icon el_icon__interviewer'></div>
