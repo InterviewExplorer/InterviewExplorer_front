893e5089a04ef6d5feb6dde7c61fbe9c6693ce01
duri22 <myukonglove@gmail.com>
2024-09-25 10:15:16 +0900

    keyword search

--- src/v2/Interviewer.js
+++ src/v2/Interviewer.js
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 
 function Interviewer() {
     const [files, setFiles] = useState([]);
-
+    const [query, setQuery] = useState('');
+    const handleChange = (event) => setQuery(event.target.value);
     const handleFileChange = (event) => {
         const selectedFiles = Array.from(event.target.files);
         setFiles(selectedFiles);
@@ -12,6 +13,48 @@ function Interviewer() {
         console.log('Uploaded files:', files);
     };
 
+    const submitQuery = async (value) =>{
+        const formData = new FormData();
+        formData.append("query",query);
+        try {
+            
+            if(value=="키워드"){const response = await fetch(`http://localhost:8000/search_resumes_nori`, {
+                method: 'POST',
+                body: formData,
+            });
+            const data = await response.json(); // JSON 데이터로 변환
+            setFiles(data); // 받아온 배열 데이터를 상태로 저장
+        }
+        else if(value=="벡터"){
+            const response = await fetch(`http://localhost:8000/search_resumes`, {
+                method: 'POST',
+                body: formData,
+            });
+            const data = await response.json(); // JSON 데이터로 변환
+            setFiles(data); // 받아온 배열 데이터를 상태로 저장
+        }
+     }catch (error) {
+         console.error('에러 발생:', error);
+         
+     }
+
+    
+    
+    
+    
+}
+const renderObject = (obj) => {
+    return (
+      <ul>
+        {Object.entries(obj).map(([key, value]) => (
+          <li key={key}>
+            <strong>{key} : {value}</strong>
+            
+          </li>
+        ))}
+      </ul>
+    );
+  }; 
     return (
         <>
             <input 
@@ -20,14 +63,25 @@ function Interviewer() {
                 multiple 
                 onChange={handleFileChange} 
             />
-            <button onClick={handleUpload}></button>
+            <input type="text" onChange={handleChange}/>
+            <button type="submit" onClick={()=>submitQuery("키워드")}>키워드검색</button>
+            {/* <button type="submit" onClick={()=>submitQuery("벡터")}>벡터검색</button> */}
+            
             <div>
                 <h3>Selected Files:</h3>
                 <ul>
                     {files.map((file, index) => (
-                        <li key={index}>{file.name}</li>
+                        <>
+                        {/* <li key={index}>{file.source}</li> */}
+                        <li key={index}>{file.score}</li>
+                        {file.resume&&<li key={index}>{file.resume}</li>}
+                        </>
+                        
+                        
+                        
                     ))}
                 </ul>
+                
             </div>
         </>
     );
