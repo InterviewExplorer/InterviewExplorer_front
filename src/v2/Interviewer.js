84b0769d87dc9ad69b3265bbed3dbe7b89398e41
LXXDJ <joker96.dj@gmail.com>
2024-10-01 13:03:23 +0900

    #25 면접관 버전: 업로드, 검색 페이지 분리

--- src/v2/Interviewer.js
+++ src/v2/Interviewer.js
@@ -1,12 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import Loading from '../pages/Loading';
 
 function Interviewer() {
+    const [summaryData, setSummaryData] = useState({});
+    const [loading, setLoading] = useState(false);
+    const location = useLocation();
     const [files, setFiles] = useState([]);
     const [query, setQuery] = useState('');
-    const [loading, setLoading] = useState(false);
-    const [summaryData, setSummaryData] = useState({});
-    const [isFileUploaded, setIsFileUploaded] = useState(false);
+    const [isFileUploaded, setIsFileUploaded] = useState(true);
     const [selectedOptions, setSelectedOptions] = useState([]);
     const [isResultActive, setIsResultActive] = useState(false);
     const [selectedResume, setSelectedResume] = useState(null);
@@ -14,54 +16,18 @@ function Interviewer() {
     const [error, setError] = useState(null);
     const [pdfFiles, setPdfFiles] = useState({});
 
-    const handleChange = (event) => setQuery(event.target.value);
-
-    const handleFileChange = async (event) => {
-        const selectedFiles = Array.from(event.target.files);
-        setFiles(selectedFiles);
-        setIsFileUploaded(true);
-    
-        const formData = new FormData();
-        const newPdfFiles = {};
-        selectedFiles.forEach((file) => {
-            formData.append(`files`, file);
-            formData.append(`sources`, file.name);
-            newPdfFiles[file.name] = file;
-        });
-        setPdfFiles(newPdfFiles);
-    
-        try {
-            setLoading(true);
-            setError(null);
-    
-            // Reset index first
-            const clearResponse = await fetch("http://localhost:8000/reset_index", {
-                method: 'POST',
-            });
-            if (!clearResponse.ok) {
-                throw new Error('인덱스 초기화 중 오류가 발생했습니다.');
+    useEffect(() => {
+        if (location.state) {
+            if (location.state.pdfData) {
+                setSummaryData(location.state.pdfData);
             }
-
-            // Then upload PDF files
-            const response = await fetch('http://localhost:8000/pdf', {
-                method: 'POST',
-                body: formData,
-            });
-    
-            if (!response.ok) {
-                const errorData = await response.json();
-                throw new Error(errorData.detail || '파일 업로드 중 오류가 발생했습니다.');
+            if (location.state.newPdfFiles) {
+                setPdfFiles(location.state.newPdfFiles);
             }
-    
-            const data = await response.json();
-            setSummaryData(data);
-        } catch (error) {
-            console.error('오류:', error);
-            setError(error.message);
-        } finally {
-            setLoading(false);
         }
-    };
+    }, [location.state]);
+
+    const handleChange = (event) => setQuery(event.target.value);
 
     const handleCheckboxChange = (event) => {
         const value = event.target.value;
@@ -110,7 +76,6 @@ function Interviewer() {
     };
 
     const handlePdfView = (fileName) => {
-        console.log('Attempting to open PDF with fileName:', fileName);
         const file = pdfFiles[fileName];
         if (file) {
             const fileUrl = URL.createObjectURL(file);
@@ -133,7 +98,7 @@ function Interviewer() {
     
         const skillCategories = skills.split('/ ').reduce((acc, item) => {
             const [category, skillList] = item.split(': ');
-            if (skillList && skillList.trim() !== '') {  // 스킬 리스트가 있고 비어있지 않은 경우만 추가
+            if (skillList && skillList.trim() !== '') {
                 acc[category] = skillList.trim();
             }
             return acc;
@@ -168,114 +133,101 @@ function Interviewer() {
                 <Loading />
             ) : (
             <div className='ly_maxWd hp_pt50 hp_pb50'>
-                {isFileUploaded ? (                    
-                    <>
-                        <div className='bl_search el_box'>
-                            <input className='bl_search__input' type="text" onChange={handleChange}/>
-                            <button className='bl_search__btn el_btnGradation' type="submit" onClick={()=>submitQuery("벡터")}><b className='WA'>검색</b></button>
-                        </div>
-                        <div className='bl_exprience ly_flexC ly_fitemC hp_mt20'>
-                            <span className='bl_exprience__ttl hp_36Color'>경력</span>
-                            <label className="container">
-                                <input type="checkbox" value="신입" onChange={handleCheckboxChange} />
-                                <span className="checkmark"></span> 신입
-                            </label>
-                            <label className="container">
-                                <input type="checkbox" value="1년이상 3년미만" onChange={handleCheckboxChange} />
-                                <span className="checkmark"></span> 1 ~ 3년
-                            </label>
-                            <label className="container">
-                                <input type="checkbox" value="3년이상 5년미만" onChange={handleCheckboxChange} />
-                                <span className="checkmark"></span> 3 ~ 5년
-                            </label>
-                            <label className="container">
-                                <input type="checkbox" value="5년이상 7년미만" onChange={handleCheckboxChange} />
-                                <span className="checkmark"></span> 5 ~ 7년
-                            </label>
-                            <label className="container">
-                                <input type="checkbox" value="7년이상 10년미만" onChange={handleCheckboxChange} />
-                                <span className="checkmark"></span> 7 ~ 10년
-                            </label>
-                            <label className="container">
-                                <input type="checkbox" value="10년이상" onChange={handleCheckboxChange} />
-                                <span className="checkmark"></span> 10년이상
-                            </label>
-                        </div>
-                        <div className='ly_flex ly_fitemC hp_mt50'>
-                                <p className='hp_fs22'>총 {summaryData.length}개</p>
-                                <div className="filebox hp_ml20">
-                                    {/* <label htmlFor="file" className='el_btnXS el_btn0Back'>+ 이력서 추가</label>  */}
-                                    {/* <input type="file" id="file" accept=".pdf" multiple onChange={handleFileChange} style={{display: 'none'}}/> */}
-                                </div>
-                            </div>
-                            <div className='ly_flex ly_fitemStart hp_mt20 el_box el_box__result'>
-                                <div className={`bl_result  ${isResultActive ? 'hp_on' : ''}`}>
-                                    <ul className='bl_resumeList'>
-                                    {Object.entries(summaryData).map(([id, resume]) => (
-                                            <li 
-                                                key={id}
-                                                onClick={() => handleListItemClick(id)}
-                                                className={activeResumeId === id ? 'hp_on' : ''}
-                                            >
-                                                {resume.name} ({resume.date_of_birth})
-                                            </li>
-                                        ))}
-                                    </ul>
-                                </div>
-                                <div className={`bl_resumeSummary el_box ${isResultActive ? 'hp_on' : ''}`}>
-                                    {selectedResume && (
-                                        <>
-                                            <table>
-                                                <tbody>
-                                                <tr>
-                                                    <th>이름</th>
-                                                    <td>{selectedResume.name || '정보 없음'}</td>
-                                                </tr>
-                                                <tr>
-                                                    <th>생년월일</th>
-                                                    <td>{selectedResume.date_of_birth || '정보 없음'}</td>
-                                                </tr>
-                                                <tr>
-                                                    <th>프로젝트수</th>
-                                                    <td>{selectedResume.number_of_projects || '정보 없음'}</td>
-                                                </tr>
-                                                <tr>
-                                                    <th>프로젝트명</th>
-                                                    <td>{selectedResume.project_description || '정보 없음'}</td>
-                                                </tr>
-                                                <tr>
-                                                    <th>키워드</th>
-                                                    <td>{selectedResume.summary_keywords || '정보 없음'}</td>
-                                                </tr>
-                                                <tr>
-                                                    <th>경력</th>
-                                                    <td>{selectedResume.work_experience || '정보 없음'}</td>
-                                                </tr>
-                                                </tbody>
-                                            </table>
-                                            <h3 className='hp_purpleColor hp_fs18 hp_mt40 hp_mb10'>보유기술</h3>
-                                            <table className="inner-table">
-                                                <tbody>
-                                                    <TechnicalSkillsRows skills={selectedResume.technical_skills} />
-                                                </tbody>
-                                            </table>
-                                        </>
-                                    )}
-                                    <button 
-                                    className='bl_resumeSummary__btn el_btnM el_btnSkyBord hp_mt50 hp_w100' 
-                                    onClick={() => handlePdfView(selectedResume.source)}
-                                    > PDF 원본보기 </button>
-                                </div>
-                            </div>
-                        </>
-                ) : (
-                    <div className="filebox hp_alignC">
-                        <label htmlFor="file" className='el_uploadBtn el_btnL el_btn0Back hp_fontGmarket'>이력서 업로드</label> 
+                <div className='bl_search el_box'>
+                    <input className='bl_search__input' type="text" onChange={handleChange}/>
+                    <button className='bl_search__btn el_btnGradation' type="submit" onClick={()=>submitQuery("벡터")}><b className='WA'>검색</b></button>
+                </div>
+                <div className='bl_exprience ly_flexC ly_fitemC hp_mt20'>
+                    <span className='bl_exprience__ttl hp_36Color'>경력</span>
+                    <label className="container">
+                        <input type="checkbox" value="신입" onChange={handleCheckboxChange} />
+                        <span className="checkmark"></span> 신입
+                    </label>
+                    <label className="container">
+                        <input type="checkbox" value="1년이상 3년미만" onChange={handleCheckboxChange} />
+                        <span className="checkmark"></span> 1 ~ 3년
+                    </label>
+                    <label className="container">
+                        <input type="checkbox" value="3년이상 5년미만" onChange={handleCheckboxChange} />
+                        <span className="checkmark"></span> 3 ~ 5년
+                    </label>
+                    <label className="container">
+                        <input type="checkbox" value="5년이상 7년미만" onChange={handleCheckboxChange} />
+                        <span className="checkmark"></span> 5 ~ 7년
+                    </label>
+                    <label className="container">
+                        <input type="checkbox" value="7년이상 10년미만" onChange={handleCheckboxChange} />
+                        <span className="checkmark"></span> 7 ~ 10년
+                    </label>
+                    <label className="container">
+                        <input type="checkbox" value="10년이상" onChange={handleCheckboxChange} />
+                        <span className="checkmark"></span> 10년이상
+                    </label>
+                </div>
+                <div className='ly_flex ly_fitemC hp_mt50'>
+                    <p className='hp_fs22'>총 {Object.keys(summaryData).length}개</p>
+                    {/* <div className="filebox hp_ml20">
+                        <label htmlFor="file" className='el_btnXS el_btn0Back'>+ 이력서 추가</label> 
                         <input type="file" id="file" accept=".pdf" multiple onChange={handleFileChange} style={{display: 'none'}}/>
-                        <p className='hp_fontGmarket hp_fs30 hp_mt50'>지원자들의 이력서를 모두 업로드 해주세요.</p>
-                        <p className='hp_fontGmarket hp_fs22 hp_mt10'>※ 업로드 양에 따라 분석하는데 시간이 더 소요될 수 있습니다.</p>
-                    </div>                    
-                )}
+                    </div> */}
+                </div>
+                <div className='ly_flex ly_fitemStart hp_mt20 el_box el_box__result'>
+                    <div className={`bl_result  ${isResultActive ? 'hp_on' : ''}`}>
+                        <ul className='bl_resumeList'>
+                        {Object.entries(summaryData).map(([id, resume]) => (
+                                <li 
+                                    key={id}
+                                    onClick={() => handleListItemClick(id)}
+                                    className={activeResumeId === id ? 'hp_on' : ''}
+                                >
+                                    {resume.name} ({resume.date_of_birth})
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                    <div className={`bl_resumeSummary el_box ${isResultActive ? 'hp_on' : ''}`}>
+                        {selectedResume && (
+                            <>
+                                <table>
+                                    <tbody>
+                                    <tr>
+                                        <th>이름</th>
+                                        <td>{selectedResume.name || '정보 없음'}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>생년월일</th>
+                                        <td>{selectedResume.date_of_birth || '정보 없음'}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>프로젝트수</th>
+                                        <td>{selectedResume.number_of_projects || '정보 없음'}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>프로젝트명</th>
+                                        <td>{selectedResume.project_description || '정보 없음'}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>키워드</th>
+                                        <td>{selectedResume.summary_keywords || '정보 없음'}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>경력</th>
+                                        <td>{selectedResume.work_experience || '정보 없음'}</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                                <h3 className='hp_purpleColor hp_fs18 hp_mt40 hp_mb10'>보유기술</h3>
+                                <table className="inner-table">
+                                    <tbody>
+                                        <TechnicalSkillsRows skills={selectedResume.technical_skills} />
+                                    </tbody>
+                                </table>
+                            </>
+                        )}
+                        <button className='bl_resumeSummary__btn el_btnM el_btnSkyBord hp_mt50 hp_w100' 
+                        onClick={() => handlePdfView(selectedResume.source)} > PDF 원본보기 </button>
+                    </div>
+                </div>
             </div>
             )}
         </div>
