742cbc48b8e4ca761ada945053ef7447bbd484e5
duri22 <myukonglove@gmail.com>
2024-10-05 21:26:38 +0900

    면접관영상추가

--- src/v2/LatestQuestions.js
+++ src/v2/LatestQuestions.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-function LatestQuestions({ job, type, answers, handleQuestion, questions }) {
+function LatestQuestions({ job, type, answers, handleQuestion, questions ,handleInterviewerUpdate}) {
     const [hasAnswer, setHasAnswer] = useState(false);
 
     useEffect(() => {        
@@ -37,14 +37,33 @@ function LatestQuestions({ job, type, answers, handleQuestion, questions }) {
 
                     const data = await response.json();
                     const LastestQuestion = { Q8: data.Questions };
-
+                    followingInterviewers('Q8',data.questions)
+                    console.log(data.Questions)
+                    console.log(data.questions)
                     handleQuestion(LastestQuestion);
                     
                 } catch (error) {
                     console.error('새 질문 생성 중 오류 발생:', error);
                 }
             };
-
+            const followingInterviewers =async (questionNum, generatedQuestion) =>{
+                const formData = new FormData();
+                //키와 생성된 문제를 formdata 에 추가
+                formData.append(questionNum,generatedQuestion);
+        
+                //꼬리질문에 대한 영상 생성
+                const response = await fetch('http://localhost:8000/ai-presenter/',{
+                    method : 'POST',
+                    body: formData
+                });
+                if (!response.ok) {
+                    throw new Error('영상 생성에 실패했습니다.');
+                }
+        
+                const data2 = await response.json();
+                console.log("데이터잘옴?",data2)
+                handleInterviewerUpdate(data2)
+            }     
             fetchNewQuestion();
             setHasAnswer(false);
         }
